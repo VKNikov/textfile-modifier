14ca85eeb974267d3dd18ec6722b805e23696b5a
Vassil <vassiln@yahoo.com>
2018-01-02 14:34:10 +0200

    Created a simple command line tool to replace a specific string in text files using regex.

---
+++ src/com/filemodifier/Main.java
@@ -0,0 +1,53 @@
+package com.filemodifier;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter full path to target directory and press enter:");
+            String folderPath = scanner.nextLine();
+            System.out.println("Enter name of file to search for and press enter:");
+            String fileName = scanner.nextLine();
+
+            List<Path> filePaths = Files.walk(Paths.get(folderPath))
+                    .filter(Files::isRegularFile)
+                    .filter(x -> x.getFileName().toString().equals(fileName))
+                    .collect(Collectors.toList());
+            if (filePaths.isEmpty()) {
+                System.out.println(String.format("No files with name %s found.", fileName));
+            } else {
+                for (Path file : filePaths) {
+                    findAndChangeString(file, scanner);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Oops. It seems There is no such directory: " + e.getMessage() + ". Please try again.");
+        }
+    }
+
+
+    private static void findAndChangeString(Path file, Scanner scanner) {
+        System.out.println("Enter regex pattern to look for and press enter:");
+        String pattern = scanner.nextLine();
+        System.out.println("Enter replacement string and press enter: ");
+        String newText = scanner.nextLine();
+        try (Stream<String> lines = Files.lines(file)) {
+            List<String> modifiedLines = lines.map(x -> x.replaceAll(pattern, newText))
+                    .collect(Collectors.toList());
+            Files.write(file, modifiedLines);
+
+        } catch (IOException e) {
+            System.out.println("Something went wrong while reading and writing to the file. Please try again.");
+            e.printStackTrace();
+        }
+    }
+}
